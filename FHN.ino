6a5002f89bdf2c1c27751ae3652c22a669b30b25
ant <ant@toplap>
2014-05-30 11:50:43 +0100

    Initial commit

---
+++ FHN.ino
@@ -0,0 +1,12 @@
+// FHN.ino - 28/05/2014
+// Tangible Networks
+// Antos
+//
+// Fitzhugh-Nagumo (FHN) model of neuronal spiking.
+// The FHN model is a two-dimensional simplification of the Hodgkin-Huxley model.
+// 
+// #TODO: Write some other stuff.
+//
+// See for example http://www.scholarpedia.org/article/FitzHugh-Nagumo_model
+//
+
